dcd9925c9c041e3dcd7e25a3f6f09b55e8c96886
Quinn Daley <quinn@fishpercolator.co.uk>
2024-01-21 17:20:51 +0000

    The most basic timer to begin with

--- timer.py
+++ timer.py
@@ -1,9 +1,12 @@
 from maubot import Plugin, MessageEvent
 from maubot.handlers import command
-
+import asyncio
 
 class Timer(Plugin):
   @command.new()
   @command.argument("time")
   async def timer(self, evt: MessageEvent, time: str) -> None:
+    time = int(time)
     await evt.respond(f"Timer started for {time} seconds")
+    await asyncio.sleep(time)
+    await evt.respond("Times up!")
