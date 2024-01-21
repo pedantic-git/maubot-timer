30dd9494d91bfd23863f90e5b942e07a27e715bc
Quinn Daley <quinn@fishpercolator.co.uk>
2024-01-21 18:36:29 +0000

    Allow timers to be named

--- timer.py
+++ timer.py
@@ -4,9 +4,11 @@ import asyncio
 
 class Timer(Plugin):
   @command.new()
-  @command.argument("time")
-  async def timer(self, evt: MessageEvent, time: str) -> None:
+  @command.argument("time", required=True)
+  @command.argument("name", required=False)
+  async def timer(self, evt: MessageEvent, time: str, name: str | None) -> None:
     time = int(time)
-    await evt.respond(f"Timer started for {time} seconds")
+    name = name or "anon"
+    await evt.respond(f"⏳ Timer {name} started for {time} seconds")
     await asyncio.sleep(time)
-    await evt.respond("Times up!")
+    await evt.respond(f"⏰ Timer {name}: time's up!")
